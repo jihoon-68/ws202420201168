c158998f409e4816ea8e8dc4d7864fd2887f2d02
jihoon <pkrwlgns64@gmail.com>
2024-08-02 00:43:37 +0900

    2024-08-02-12:43

--- day05/server/app.js
+++ day05/server/app.js
@@ -1,108 +1,108 @@
 const http = require("http");
-const express = require('express');
-const app =express();
+const express = require("express");
+const app = express();
 
-const port =3000;
+const port = 3000;
 
-app.set('views', 'views');
-app.set('view engine', 'ejs');
-
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 //paudlic/index.html을 보여주기 위한 명령어
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-let saramList=[
-    {no:102, name:'홍길동', email:'hong@saram.com', job:'도둑', age:32},
-    {no:103, name:'이길동', email:'lee@saram.com', job:'변호사',age:24},
-    {no:103, name:'김길순', email:'kim@saram.com', job:'프로그래머',age:34},
-    {no:104, name:'박길순', email:'park@saram.com', job:'군인',age:41}
-]
+const saramList = [
+  { no: 102, name: "홍길동", email: "hong@saram.com", job: "도둑", age: 32 },
+  { no: 103, name: "이길동", email: "lee@saram.com", job: "변호사", age: 24 },
+  {
+    no: 103,
+    name: "김길순",
+    email: "kim@saram.com",
+    job: "프로그래머",
+    age: 34,
+  },
+  { no: 104, name: "박길순", email: "park@saram.com", job: "군인", age: 41 },
+];
 
 //saram
-app.get('/saram', function(req,res){
-    //ejs 페이지로 렌더링
-    // -views/saram.esj 페이지의 로딩
-    
+app.get("/saram", function (req, res) {
+  //ejs 페이지로 렌더링
+  // -views/saram.esj 페이지의 로딩
 
-    var message_data ="사람 정보 관리 페이지";
-    req.app.render('saram',{message_data,saramList}, function(err, html){
-        res.end(html);
-    });
+  var message_data = "사람 정보 관리 페이지";
+  req.app.render("saram", { message_data, saramList }, function (err, html) {
+    res.end(html);
+  });
 });
-app.get('/saram/detail',function(req,res){
-    console.log();
-    var idx = saramList.findIndex(function(saram){
-        
-        return saram.no == req.query.no;
-    });
+app.get("/saram/detail", function (req, res) {
+  console.log();
+  var idx = saramList.findIndex(function (saram) {
+    return saram.no == req.query.no;
+  });
 
-    var saram =null;
+  var saram = null;
 
-    if(idx != -1 ){
-        saram = saramList[idx];
-    }
-    
-    req.app.render('saramDetail',{saram}, function(err, html){
-        res.end(html);
-    });
+  if (idx != -1) {
+    saram = saramList[idx];
+  }
+
+  req.app.render("saramDetail", { saram }, function (err, html) {
+    res.end(html);
+  });
 });
 
-app.get('/saram/edit', function(req, res) {
-    console.log("GET - /saram/edit >>>> no: " + req.query.no);
-    var idx = saramList.findIndex(function(saram) {
-        return saram.no == req.query.no;
-    });
-    var saram = null;
-    if(idx != -1) {
-        saram = saramList[idx];
-    }
-    req.app.render('saramEdit', {saram}, function(err, html) {
-        res.end(html);
-    });
+app.get("/saram/edit", function (req, res) {
+  console.log("GET - /saram/edit >>>> no: " + req.query.no);
+  var idx = saramList.findIndex(function (saram) {
+    return saram.no == req.query.no;
+  });
+  var saram = null;
+  if (idx != -1) {
+    saram = saramList[idx];
+  }
+  req.app.render("saramEdit", { saram }, function (err, html) {
+    res.end(html);
+  });
 });
 
-app.get('/saram/update', function(req, res) {
-    console.log("GET - /saram/update >>> ", req.query);
-    // saramList에서 해당 정보를 찾아서 update 하기.
+app.get("/saram/update", function (req, res) {
+  console.log("GET - /saram/update >>> ", req.query);
+  // saramList에서 해당 정보를 찾아서 update 하기.
 
-    var idx = saramList.findIndex(function(saram) {
-        return saram.no == req.query.no;
-    });
+  var idx = saramList.findIndex(function (saram) {
+    return saram.no == req.query.no;
+  });
 
-    if(idx != -1) {
-        saramList[idx] =req.query;
-        /*saramList[idx].name = req.query.name;
+  if (idx != -1) {
+    saramList[idx] = req.query;
+    /*saramList[idx].name = req.query.name;
         saramList[idx].email = req.query.email;
         saramList[idx].job = req.query.job;
         saramList[idx].age = req.query.age;*/
+  }
+  res.redirect("/saram");
+});
+
+app.get("/saram/delete", function (req, res) {
+  console.log("GET - /saram/delete >>> ", req.query);
+  // saramList에서 해당 정보를 찾아서 update 하기.
+  var List = [];
+  saramList.findIndex(function (saram) {
+    if (saram.no != req.query.no) {
+      List.push(saram);
     }
-    res.redirect('/saram');
-    
+  });
+
+  if (List != -1) {
+    saramList = List;
+  }
+  res.redirect("/saram");
 });
 
-app.get('/saram/delete', function(req, res) {
-    console.log("GET - /saram/delete >>> ", req.query);
-    // saramList에서 해당 정보를 찾아서 update 하기.
-    var List =[];
-    saramList.findIndex(function(saram) {
-        if(saram.no != req.query.no){
-            List.push(saram);
-        }
-    });
-
-    if(List != -1) {
-        saramList = List;
-        
-    }
-    res.redirect('/saram');
-    
+app.get("/profile", function (req, res) {
+  res.end("<h1> profile</h1>");
 });
 
-app.get('/profile', function(req,res){
-    res.end('<h1> profile</h1>')
-});
-
-const server =http.createServer(app);
-server.listen(3000, function(){
-    console.log("서버실행중>>> http://localhost:"+port)
+const server = http.createServer(app);
+server.listen(3000, function () {
+  console.log("서버실행중>>> http://localhost:" + port);
 });
--- day06/server/app.js
+++ day06/server/app.js
@@ -1,56 +1,60 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const methodOverride = require("method-override");
 // npm i -S body-parser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.set('views', path.join(__dirname, 'views') );
-app.set('view engine', 'ejs');
-app.set('port', 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("port", 3000);
 
 // bodyParser 미들웨어 지정 - POST방식의 파라미터 사용 가능.
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-    // redirect() 지정 된 URL로 자동으로 페이지 이동
-    // res.redirect('http://naver.com');
+  // redirect() 지정 된 URL로 자동으로 페이지 이동
+  // res.redirect('http://naver.com');
 
-    // end() 문자열을 화면에 출력한다.
-    //res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
-    //res.end("안영 세계");
-    res.end('{"hello":"world"}');
+  // end() 문자열을 화면에 출력한다.
+  //res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
+  //res.end("안영 세계");
+  res.end('{"hello":"world"}');
 
-    var obj = {no:120, name:"HONG"};
-    //res.end(JSON.stringify(obj) );
-    // send()는 수식 or 문자열을 화면에 보여 준다.
-    // writeHead()는 생략.
-    //res.send(obj); 
+  var obj = { no: 120, name: "HONG" };
+  //res.end(JSON.stringify(obj) );
+  // send()는 수식 or 문자열을 화면에 보여 준다.
+  // writeHead()는 생략.
+  //res.send(obj);
 });
 
 // localhost:3000/data/hong/love  ==> get('/data/:user/:message)
 // localhost:3000/data?user=HONG&message=LOVE
-app.get('/data', (req, res) => {
-    // POST 방식에는 body, 패스파라미터 방식 params, GET 빙식에는 query 객체로 전달...
-    // POST 방식에서는 bodyParser 미들웨어 설정 필수.
-    const user = req.query.user;
-    const message = req.query.message;
-    const jsonData = {user, message};
+app.get("/data", (req, res) => {
+  // POST 방식에는 body, 패스파라미터 방식 params, GET 빙식에는 query 객체로 전달...
+  // POST 방식에서는 bodyParser 미들웨어 설정 필수.
+  const user = req.query.user;
+  const message = req.query.message;
+  const jsonData = { user, message };
 
-    res.send(jsonData);
+  res.send(jsonData);
 });
 
 // 임시 todoList 데이터 저장
 const todoList = [
-    {no: 101, title:'자연 보호 하기', done:true},
-    {no: 102, title:'엄마 생일 선물', done:false},
-    {no: 103, title:'아빠 집 사주기', done:true},
-    {no: 104, title:'취직 하기', done:false},
-    {no: 104, title:'여친 부모님 여행 시켜주기', done:false},
+  { no: 101, title: "자연 보호 하기", done: true },
+  { no: 102, title: "엄마 생일 선물", done: false },
+  { no: 103, title: "아빠 집 사주기", done: true },
+  { no: 104, title: "취직 하기", done: false },
+  { no: 105, title: "여친 부모님 여행 시켜주기", done: false },
 ];
+
+var noSeq = 106;
 
 // AJAX를 REST 방식으로 처리 (HTML 폼은 GET과 POST만 가능)
 // GET - 출력, 검색
@@ -60,51 +64,122 @@ const todoList = [
 // FETCH - 부분수정
 // ... 그 외
 
-//처음 정보 상세정보
-app.get('/todo', (req, res) => {
-    
-    if(req.query.no){
-        var no = req.query.on;
-        var idx = todoList.findIndex((t)=>{
-            return t.no == no;
-        });
-        if(idx){
-            req.app.render('todo',{todoList}, function(err,html){
-                res.send(todoList[idx]);
-                res.end(html);
-            });
-            console.log(1);
-        }else{
-            res.send(null);
-            console.log(2)
+//  검색
+app.get("/todo/search", (req, res) => {
+  var keyword = req.query.keyword;
+  var newTodoList = todoList.filter((todo) => {
+    return todo.title.findIndex(keyword) != -1;
+  });
+  res.send(newTodoList);
+});
+
+//  상세보기 or 전체보기
+app.get("/todo", (req, res) => {
+  //console.log(req.query);
+  if (req.query.no) {
+    var no = req.query.no;
+    var idx = todoList.findIndex((t) => {
+      return t.no == no;
+    });
+    //console.log(idx);
+    if (idx != -1) {
+      //console.log(1);
+      //console.log(todo);
+      req.app.render(
+        "todoDetail",
+        { todo: todoList[idx] },
+        function (err, html) {
+          res.end(html);
         }
-        return;
+      );
+    } else {
+      res.send(null);
     }
-    console.log(3)
-    req.app.render('todo',{todoList}, function(err, html){
-        res.end(html);
-    });
-    
-});
-//검색
-app.post('/todo/search', (req, res) => {
-    var keyword = req.query.keyword;
-    var newTodoList = todoList.filter((todo)=>{
-        return todo.title.findlndex(keyword) != -1;
-    });
-    res.send(todoList);
+    return;
+  }
+  //console.log(3);
+
+  //res.send(todoList);
+  req.app.render("todo", { todoList }, function (err, html) {
+    res.end(html);
+  });
 });
 
-//수정
-app.put('/todo', (req, res) => {
-    res.send(todoList);
+// 입력
+app.post("/todo/input", (req, res) => {
+  var title = req.body.title;
+  todoList.push({ no: noSeq++, title, done: false });
+  res.redirect("/todo");
 });
 
-app.delete('/todo', (req, res) => {
-    res.send(todoList);
+//수정 입력 페이지
+app.get("/todo/edit", (req, res) => {
+  var no = req.query.no;
+  var idx = todoList.findIndex((t) => {
+    return t.no == no;
+  });
+  if (idx != -1) {
+    //console.log({ todo: todoList[idx] });
+    req.app.render("todoEdit", { todo: todoList[idx] }, function (err, html) {
+      res.end(html);
+    });
+    return;
+  }
+  //console.log(no);
+  res.send("정보 불러오기 오류가 있습니다");
+});
+// 수정
+app.put("/todo/updat/:no", (req, res) => {
+  console.log(1);
+  var todo = req.body;
+  //console.dir(todo);
+  var idx = todoList.findIndex((t) => {
+    return t.no == todo.no;
+  });
+  console.log(idx);
+  if (idx != -1) {
+    todoList[idx] = todo;
+    //console.log(todoList[idx]);
+    /*req.app.render("todo", { todo: todoList[idx] }, function (err, html) {
+      res.end(html);
+    });*/
+    res.redirect("/todo");
+    return;
+  }
+  res.send("정보 불러오기 오류가 있습니다");
+});
+
+//삭제 확인 페이지
+
+app.get("/todo/deleteck", (req, res) => {
+  var no = req.query.no;
+  var idx = todoList.findIndex((t) => {
+    return t.no == no;
+  });
+  if (idx != -1) {
+    //console.log({ todo: todoList[idx] });
+    req.app.render("todoDelete", { todo: todoList[idx] }, function (err, html) {
+      res.end(html);
+    });
+    return;
+  }
+  //console.log(no);
+  res.send("정보 불러오기 오류가 있습니다");
+});
+
+// 삭제
+app.delete("/todo/delete/:no", (req, res) => {
+  var no = parseInt(req.body.no);
+  var idx = todoList.findIndex((t) => {
+    return t.no == no;
+  });
+  if (idx != -1) {
+    todoList.splice(idx, 1);
+  }
+  res.redirect("/todo");
 });
 
 const server = http.createServer(app);
-server.listen(app.get('port'), () => {
-    console.log(`노드js 서버 실행 중 >>> http://localhost:${app.get('port')}`);
+server.listen(app.get("port"), () => {
+  console.log(`노드js 서버 실행 중 >>> http://localhost:${app.get("port")}`);
 });
